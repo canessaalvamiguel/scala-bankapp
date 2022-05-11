a9f2f28914a21bbf59034f6bee6a9238a048036d
Miguel Canessa <canessaalvamiguel@gmail.com>
2022-05-11 00:37:37 -0500

    Implemet PersistentBankAccount actor

---
+++ src/main/scala/com/rockthejvm/actors/PersistentBankAccount.scala
@@ -0,0 +1,76 @@
+package com.rockthejvm.actors
+
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.persistence.typed.PersistenceId
+import akka.persistence.typed.scaladsl.{Effect, EventSourcedBehavior}
+
+// a single bank account
+class PersistentBankAccount {
+
+  //commands = messages
+  sealed trait Command
+  case class CreateBankAccount(user: String, currency: String, initBalance: Double, replyTo: ActorRef[Response]) extends Command
+  case class UpdateBalance(id: String, currency: String, amount: Double,  replyTo: ActorRef[Response]) extends Command
+  case class GetBankAccount(id: String, replyTo: ActorRef[Response]) extends Command
+
+  //events = to persist to Cassandra
+  trait Event
+  case class BankAccountCreated(bankAccount: BankAccount) extends Event
+  case class BalanceUpdated(amount: Double) extends Event
+
+  //state
+  case class BankAccount(id: String, user: String, currency: String, balance: Double)
+
+  //responses
+  sealed trait Response
+  case class BankAccountCreatedResponse(id: String)  extends Response
+  case class BankAccountBalanceUpdatedResponse(maybeBankAccount: Option[BankAccount]) extends Response
+  case class GetBankAccountResponse(maybeBankAccount: Option[BankAccount]) extends Response
+
+  //command handler = message handler => persist an event
+  //event handler => update state
+  //state
+
+  val commandHandler : (BankAccount, Command) => Effect[Event, BankAccount] = (state, command) =>
+    command match {
+      case CreateBankAccount(user, currency, initBalance, bank) =>
+        /*
+          - bank creates me
+          - bank sends me CreateBankAccount
+          - I persist BankAccountCreated
+          - I update my state
+          - reply back to bank with the BankAccountCreatedResponse
+          - (the bank surfaces the response to the HTTP server)
+         */
+        val id = state.id
+        Effect
+          .persist(BankAccountCreated(BankAccount(id, user, currency, initBalance))) // persisted into Cassandra
+          .thenReply(bank)(_ => BankAccountCreatedResponse(id))
+      case UpdateBalance(_, _, amount, bank) =>
+        val newBalance = state.balance + amount
+        //check here for withdrawal
+        if (newBalance < 0) // illegal
+          Effect.reply(bank)(BankAccountBalanceUpdatedResponse(None))
+        else
+          Effect
+            .persist(BalanceUpdated(amount))
+            .thenReply(bank)(newState => BankAccountBalanceUpdatedResponse(Some(newState)))
+      case GetBankAccount(_, bank) =>
+        Effect.reply(bank)(GetBankAccountResponse(Some(state)))
+    }
+  val eventHandler : (BankAccount, Event) => BankAccount = (state, event) =>
+    event match {
+      case BankAccountCreated(bankAccount) =>
+        bankAccount
+      case BalanceUpdated(amount) =>
+        state.copy(balance = state.balance + amount)
+    }
+
+  def apply(id: String) : Behavior[Command] =
+    EventSourcedBehavior[Command, Event, BankAccount](
+      persistenceId = PersistenceId.ofUniqueId(id),
+      emptyState = BankAccount(id, "", "", 0.0),//unused
+      commandHandler = commandHandler,
+      eventHandler = eventHandler
+    )
+}
